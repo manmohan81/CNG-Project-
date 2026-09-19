b8e8c2a0b114192b2dce8fc05533f1582fa5cca4
MANMOHAN SINGH RAWAT <manmohan.rawat7@gmail.com>
2026-09-19 11:39:40 +0530

    Update gantt_app.py
    Finalization of Costing Added

--- gantt_app.py
+++ gantt_app.py
@@ -26,7 +26,8 @@ fixed_data = [
     {"Task": "Assembly", "Start": datetime(2026, 10, 10), "End": datetime(2026, 10, 15)},
     {"Task": "Dispenser Testing", "Start": datetime(2026, 10, 15), "End": datetime(2026, 10, 20)},
     {"Task": "Rain Testing Area", "Start": datetime(2026, 10, 25), "End": datetime(2026, 10, 30)},
-    {"Task": "Dispenser Certification", "Start": datetime(2026, 8, 15), "End": datetime(2026, 12, 8)},
+    {"Task": "Finalization of costing", "Start": datetime(2026, 10, 15), "End": datetime(2026, 10, 30)},
+    {"Task": "Dispenser Certification", "Start": datetime(2026, 10, 20), "End": datetime(2026, 12, 15)},
 ]
 
 df_initial = pd.DataFrame(fixed_data)
